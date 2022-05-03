be9477ecb909d061cfe09efd1485c8e21097a3eb
0Erik1 <erikpinheiroalves@gmail.com>
2022-05-03 16:52:33 -0300

    remoção do bug da validação de email

--- pags/loginPage.php
+++ pags/loginPage.php
@@ -34,11 +34,10 @@ if ((usuario.length >=1) &&
     (dominio.indexOf(".") >=1)&&
     (dominio.lastIndexOf(".") < dominio.length - 1)) {
 document.getElementById("msgemail").innerHTML="E-mail válido";
-alert("email valido");
 }
 else{
 document.getElementById("msgemail").innerHTML="<font color='red'>Email inválido </font>";
-alert("E-mail invalido");
+
 }
 }
 </script>
