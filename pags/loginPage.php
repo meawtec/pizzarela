12d6048830b357689213716a296e559bcf999eb3
0Erik1 <erikpinheiroalves@gmail.com>
2022-05-03 16:48:18 -0300

    validação de email

--- pags/loginPage.php
+++ pags/loginPage.php
@@ -20,22 +20,49 @@ header('Location:../index.php');
     <meta charset="UTF-8">
     <title>Login</title>
 
+    <script language="Javascript">
+function validacaoEmail(field) {
+usuario = field.value.substring(0, field.value.indexOf("@"));
+dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+if ((usuario.length >=1) &&
+    (dominio.length >=3) &&
+    (usuario.search("@")==-1) &&
+    (dominio.search("@")==-1) &&
+    (usuario.search(" ")==-1) &&
+    (dominio.search(" ")==-1) &&
+    (dominio.search(".")!=-1) &&
+    (dominio.indexOf(".") >=1)&&
+    (dominio.lastIndexOf(".") < dominio.length - 1)) {
+document.getElementById("msgemail").innerHTML="E-mail válido";
+alert("email valido");
+}
+else{
+document.getElementById("msgemail").innerHTML="<font color='red'>Email inválido </font>";
+alert("E-mail invalido");
+}
+}
+</script>
+
+
 </head>
 <body>
 
 <div>
     <h2>Login</h2>
 
-    <form method="post" action="loginPage.php">
-        <input type="text" name="Nome" ip="Nome" placeholder="Nome" required>
+    <form method="post" action="loginPage.php" name="f1">
+        <input type="text" name="Nome" id="Nome" placeholder="Nome" required>
         <br>
-        <input type="text" name="Email" ip="Email" placeholder="Email" required>
+        <input type="text" name="email" id="Email" onblur="validacaoEmail(f1.email)" placeholder="Email" required>
         <br>
-        <input type="password" name="Senha" ip="Senha" placeholder="Senha" required>
+        <input type="password" name="Senha" id="Senha" placeholder="Senha" required>
         <br>
-        <input type="submit" name="blogin" ip="blogin" value="Entrar">
+        <input type="submit" name="blogin" id="blogin" value="Entrar">
     </form>
     
+    <div id="msgemail"></div>
+
 </div>
+
 </body>
 </html>
