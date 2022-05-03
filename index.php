850f9b7bb7f43c407b1c4fc506eb72467765834d
MarioRan19 <mariorodriguescruz@gmail.com>
2022-05-03 16:54:50 -0300

    Barra de navegação // cabeçalho

--- index.php
+++ index.php
@@ -1,21 +1,52 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <meta charset="UTF-8">
     <title>Pizzaria</title>
 
 </head>
 <body>
-    
-<div>
-    <button type="button"> peça online</button>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+
+    <a class="navbar-brand" href="#">Pizzarela</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+      </form>
+      <br>
+        <li class="nav-item">
+
+
+          <a class="nav-link active" aria-current="page" href="pags/cardapio.php">Peça online</a>
+        </li>
+        <a class="nav-link active" aria-current="page" href="pags/carrinho.php">Carrinho</a>
+        </li>
+        <a class="nav-link active" aria-current="page" href="pags/sobrenos.php">Sobre nos</a>
+        </li>
+        <a class="nav-link active" aria-current="page" href="pags/loginPage.php">Login Temporário</a>
+        </li>
+        
+        
+    </div>
+  </div>
+</nav>
+        <button type="button"> peça online</button>
     <button type="button"> Cardapio</button>
     <button type="button"> Sobre nós</button>
     <button type="button"> carrinho</button>
     <a href="pags/loginPage.php">
         <button type="button" > entrar</button>
-    </a>
+</a>
+<div>
+    
 
 </div>
 
---
+++ pags/cardapio.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <title>Pizzaria</title>
+
+</head>
+<body>
+<div>
+<nav class="navbar navbar-dark bg-dark">
+<div class="container-fluid">
+    <a class="navbar-brand" href="#">Pizzarela</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/pags/cardapio.php">Peça Online</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+</nav>
+</div>
+        <button type="button"> peça online</button>
+    <button type="button"> Cardapio</button>
+    <button type="button"> Sobre nós</button>
+    <button type="button"> carrinho</button>
+    <a href="pags/loginPage.php">
+        <button type="button" > entrar</button>
+</a>
+<div>
+    
+
+</div>
+
+</body>
+</html>
