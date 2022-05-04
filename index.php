5986f4a6971690d0cbc98e8f9ec335ca83032127
0Erik1 <erikpinheiroalves@gmail.com>
2022-05-04 10:48:12 -0300

    removi os botões inuteis

--- index.php
+++ index.php
@@ -38,13 +38,7 @@
     </div>
   </div>
 </nav>
-        <button type="button"> peça online</button>
-    <button type="button"> Cardapio</button>
-    <button type="button"> Sobre nós</button>
-    <button type="button"> carrinho</button>
-    <a href="pags/loginPage.php">
-        <button type="button" > entrar</button>
-</a>
+      
 <div>
     
 
