0605a2306475f61cfc211bb5cf6d46fcc47f3ba0
MarioRan19 <mariorodriguescruz@gmail.com>
2022-05-04 13:49:00 -0300

    Merge branch 'main' of https://github.com/meawtec/pizzarela

--- index.php
+++ index.php
@@ -73,7 +73,7 @@
       <div class="card" style="width: 18rem;" id="cardpromo">
       <img src="..." class="card-img-top" alt="...">
       <div class="card-body">
-      <p class="card-text">Aqui jas uma Pizza muito gostosa hmmmmmm.</p>
+      <p class="card-text">Aqui jas uma Pizza muito gostosa hmmmmm.</p>
       <button type="button" class="btn btn-success" href="/pags/carrinho.php">Ver</button>
       </div>
       </div><div class="card" style="width: 18rem;" id="cardpromo">
