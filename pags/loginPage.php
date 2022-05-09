29366993280d97d25949028f36b234313e630cd5
MarioRan19 <mariorodriguescruz@gmail.com>
2022-05-09 11:56:42 -0300

    desing do login

--- pags/loginPage.php
+++ pags/loginPage.php
@@ -46,14 +46,15 @@ else{
 }
 </script>
 
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="../estilo/estiloLogin.css">
 
 </head>
 <body>
 
 
 
-<div>
+<div id="divlogin">
     <h2>Login</h2>
 
     <form method="post" action="loginPage.php" name="f1" id="form">
