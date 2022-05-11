5efc020f45a6823e76878567197782f271180252
MarioRan19 <mariorodriguescruz@gmail.com>
2022-05-11 14:11:42 -0300

    configurando a lista cardapio

--- pags/carrinho.php
+++ pags/carrinho.php
@@ -44,26 +44,12 @@
 <div>
 <div id="divpromo">
       <div id="promocoes">
-      <div id="retapromo">
+      <div id="caixa" class="row">
+        
 
       
 
-      <div id="retapromo">
-
-      <div id="cardapio" class=" mb-3 navbar navbar-expand-lg  bg-success " >
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="../imagens/refrigerante.png" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">PA RAPARAPARAYdao</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div>
+     
 
 
 <div id="cardapio" class="card d-block card mb-3  navbar navbar-expand-lg  bg-success" style=" ">
@@ -75,34 +61,33 @@
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        
       </div>
     </div>
   </div>
 </div>
 
 
-<div id="cardapio" class="card mb-3  card mb-3 navbar navbar-expand-lg  bg-success" style="max-width: 1540px;">
+<div id="cardapio" class=" mb-3 navbar navbar-expand-lg  bg-success " >
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="../imagens/refrigerante.png" class="img-fluid rounded-start" alt="...">
+      <img src="../imagens/Refri.png" class="img-fluid rounded-start" alt="...">
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">PA RAPARAPARAYdao</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        
       </div>
     </div>
   </div>
 </div>
-      </div><br>
 
-      
-    </div>
 </div>
 
 </div>
+
+
 
 </body>
 </html>
