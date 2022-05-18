24f713ff270979252ecd1d378ae4beeacb0825c9
MarioRan19 <mariorodriguescruz@gmail.com>
2022-05-18 11:29:31 -0300

    botoes coloridos

--- pags/cardapio.php
+++ pags/cardapio.php
@@ -4,6 +4,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 <link href="estilo/estilo1.css" rel="stylesheet">
+
     <meta charset="UTF-8">
     <title>Pizzaria</title>
 
@@ -41,10 +42,7 @@
   </div>
 </nav>
 
-<div>
-    
 
-</div>
 
 </body>
 </html>
