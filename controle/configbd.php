6bbedcafb4d1836251148b9c870fd33751379b13
0Erik1 <erikpinheiroalves@gmail.com>
2022-05-24 15:33:27 -0300

    teste

--- controle/configbd.php
+++ controle/configbd.php
@@ -7,11 +7,11 @@ $dbName = "meaw";
 
 $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 
-/*if($conexao->connect_errno){
+if($conexao->connect_errno){
     echo "erro";
 
 }
 else{
     echo "deu certo <br>";
 }
-?>*/
+?>
