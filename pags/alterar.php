c413c3ce4f6ece01751e9a2a8d5a6c36c4baa865
MarioRan19 <mariorodriguescruz@gmail.com>
2022-05-30 14:49:46 -0300

    Erick baitola não faz o botão pra mim

--- pags/alterar.php
+++ pags/alterar.php
@@ -1,7 +1,7 @@
-<?php  
+<?php
 include_once '../controle/configbd.php';
 
-$select = "SELECT * FROM tbadmin ORDER BY id DESC"; 
+$select = "SELECT * FROM tbadmin ORDER BY id DESC";
 
 $Sresult = $conexao->query($select);
 
@@ -10,6 +10,7 @@ $Sresult = $conexao->query($select);
 
 <!DOCTYPE html>
 <html lang="utf-8">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,33 +18,38 @@ $Sresult = $conexao->query($select);
     <title>Document</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../estilo/estiloadm.css">
 </head>
-<body>
-    <a href="../index.php"> voltar </a>
 
-            <div>
-                <table class="table">
-    <thead>
-        <tr>
-            <th scope="col">id</th>
-            <th scope="col">nome</th>
-            <th scope="col">email</th>
-            <th scope="col">senha</th>
-            <th scope="col">adm</th>
-            <th scope="col">...</th>
-        </tr>
-    </thead>
+<body>
+    <a class="voltar" href="../index.php">
+        <button class="volt">
+            <h6>Voltar</h6>
+        </button>
+    </a>
+    <div>
+        <table class="table table-dark table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Senha</th>
+                    <th scope="col">ADM</th>
+                    <th scope="col">Alterar </th>
+                </tr>
+            </thead>
             <tbody>
-            <?php 
-            while($usardata = mysqli_fetch_assoc($Sresult)){
-            
-                echo "<tr>";
-                echo "<td>".$usardata['id']. "</td>";
-                echo "<td>".$usardata['nome']. "</td>";
-                echo "<td>".$usardata['email']. "</td>";
-                echo "<td>".$usardata['senha']. "</td>";
-                echo "<td>".$usardata['adm']. "</td>";
-                echo "<td> <a class='btn btn-sn btn-primary' href='edit.php?id=$usardata[id]'>
+                <?php
+                while ($usardata = mysqli_fetch_assoc($Sresult)) {
+
+                    echo "<tr>";
+                    echo "<td>" . $usardata['id'] . "</td>";
+                    echo "<td>" . $usardata['nome'] . "</td>";
+                    echo "<td>" . $usardata['email'] . "</td>";
+                    echo "<td>" . $usardata['senha'] . "</td>";
+                    echo "<td>" . $usardata['adm'] . "</td>";
+                    echo "<td> <a class='btn btn-sn btn-primary' href='edit.php?id=$usardata[id]'>
                 <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
                 <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
                 <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/>
@@ -57,13 +63,14 @@ $Sresult = $conexao->query($select);
 
                 <a/>
                 </td>";
-                
-                echo "</tr>";
-            }
-            
-            ?>
+
+                    echo "</tr>";
+                }
+
+                ?>
             </tbody>
         </table>
     </div>
 </body>
+
 </html>
