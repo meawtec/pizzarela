3e6cb6ab277e2f52301d0933ffb5892703eab567
MarioRan19 <mariorodriguescruz@gmail.com>
2022-06-06 14:26:52 -0300

    alterações em cores como na barra de navegação

--- pags/carrinho.php
+++ pags/carrinho.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
 
       <a class="navbar-brand" href="../index.php">Pizzarela</a>
