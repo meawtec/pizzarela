db4375ff713f7c5beaa99fee37b7424fb6f64d5c
0Erik1 <erikpinheiroalves@gmail.com>
2022-06-13 11:06:18 -0300

    sei la

--- pags/pizzas.php
+++ pags/pizzas.php
@@ -21,7 +21,6 @@ if (isset($_COOKIE['email'])) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-
   <link href="../estilo/estilo1.css" rel="stylesheet">
   <meta charset="UTF-8">
   <title>Pizzaria</title>
@@ -42,7 +41,7 @@ if (isset($_COOKIE['email'])) {
           <li class="nav-item">
 
 
-            <a class="nav-link active" aria-current="page" href="peça.php">Peça online</a>
+          <a class="nav-link active" aria-current="page" href="peça.php">Peça online</a>
           </li>
           <a class="nav-link active" aria-current="page" href="cardapio.php">Cardapio</a>
           </li>
@@ -67,25 +66,17 @@ if (isset($_COOKIE['email'])) {
           <?php
           endif
           ?>
-
-
-
           </form>
+          <h6 class="gmail">
+            <?php
 
-          <form class="d-flex">
+            if (isset($_COOKIE['email'])) {
+              echo $_COOKIE['email'];
+            }
 
-          </form>
 
-          <h6 class="gmail1">
-
-            <h6 class="gmail">
-              <?php
-
-              if (isset($_COOKIE['email'])) {
-                echo $_COOKIE['email'];
-              }
-              ?>
-               <?php
+            ?>
+             <?php
             if (isset($_COOKIE['email'])) :
             ?>
               <form action="logout.php" method="POST">
@@ -93,76 +84,18 @@ if (isset($_COOKIE['email'])) {
               </form>
 
             <?php endif; ?>
-            </h6>
-
-            <br>
+          </h6>
+          </h6>
+          <br>
 
       </div>
     </div>
   </nav>
 
   <div>
-    <div class="divpromo">
-      <div class="promocoes">
-        <div class="row">
-
-          <a href="pizzas.php" class="averde">
-            <div class="cardapio mb-3 navbar navbar-expand-lg  bg-success ">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img src="../imagens/pizza1.png" class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">Pizzas</h5>
-                    <p class="card-text">Pizas deliciosas, hmm que sabor!!</p>
 
 
-                  </div>
-                </div>
-              </div>
-            </div>
-          </a>
-
-
-          <a href="acomp.php" class="averde">
-            <div class="cardapio mb-3 navbar navbar-expand-lg  bg-success ">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img src="../imagens/Refri.png" class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">Acompanhamentos</h5>
-                    <p class="card-text">Delícia, uma delícia!</p>
-
-
-
-                  </div>
-                </div>
-              </div>
-            </div>
-          </a>
-
-          <a href="burgao.php" class="averde">
-            <div class="cardapio mb-3 navbar navbar-expand-lg  bg-success ">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img src="../imagens/ham1.png" class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">Hamburguers</h5>
-                    <p class="card-text">Hamburgueres gostosos!</p>
-
-
-                  </div>
-                </div>
-              </div>
-            </div>
-          </a>
-
-        
+  </div>
 
 </body>
 
