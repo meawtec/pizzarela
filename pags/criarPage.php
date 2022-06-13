e726ad33781a3c557a8d55df4734531ae82e6840
0Erik1 <erikpinheiroalves@gmail.com>
2022-06-13 11:33:51 -0300

    wfgsdfgjksdfsdf

--- pags/criarPage.php
+++ pags/criarPage.php
@@ -45,30 +45,6 @@ if (isset($_POST['blogin'])) {
     <meta charset="UTF-8">
     <title>Login</title>
 
-    <script language="Javascript">
-        function validacaoEmail(field) {
-            usuario = field.value.substring(0, field.value.indexOf("@"));
-            dominio = field.value.substring(field.value.indexOf("@") + 1, field.value.length);
-            if ((usuario.length >= 1) &&
-                (dominio.length >= 3) &&
-                (usuario.search("@") == -1) &&
-                (dominio.search("@") == -1) &&
-                (usuario.search(" ") == -1) &&
-                (dominio.search(" ") == -1) &&
-                (dominio.search(".") != -1) &&
-                (dominio.indexOf(".") >= 1) &&
-                (dominio.lastIndexOf(".") < dominio.length - 1)) {
-
-                document.getElementById("msgemail").innerHTML = "E-mail válido";
-                document.getElementById("form").onsubmit = (e) => e
-            } else {
-                document.getElementById("msgemail").innerHTML = "<font color='red'>Email inválido </font>";
-                document.getElementById("form").onsubmit = (e) => {
-                    e.preventDefault()
-                }
-            }
-        }
-    </script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../estilo/estiloLogin.css">
@@ -95,12 +71,12 @@ if (isset($_POST['blogin'])) {
 
            
 
-            <form method="post" action="criarPage.php" name="f1" class="form">
+            <form method="post" id="f1" action="criarPage.php" name="f1" class="form">
                 <input type="text" name="Nome" class="Nome" placeholder="Nome" required>
                 <br><br>
-                <input type="text" name="Email" class="Email" onblur="validacaoEmail(f1.email)" placeholder="Email" required>
+                <input type="email" name="Email" class="Email" placeholder="Email" required>
                 <br><br>
-                <label></label><input type="password" placeholder="Senha" name="senha" id="senha" required>             
+                <label></label><input type="password" placeholder="Senha" name="Senha" id="senha" required>             
 
 <button type="button" onclick="Toggle()">
 <svg id= "cadiado" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
